5b2ba91b35c87286781ce9906ecd4f8f9332c189
J. Ryan Kelley <ryan.kelley@espn.com>
2015-03-15 08:04:50 -0700

    Use the mobile logic, fix expanded mobile header height

--- js/dogs.js
+++ js/dogs.js
@@ -2,10 +2,7 @@ $(document).ready( function() {
 	var wrapper = $('#wrapper');
 
 	console.log("checking out ", window.outerWidth);
-	if(window.outerWidth > 800) {
-		//probably not a phone
-		wrapper.css("width", "550px");
-	} else {
+	if(window.outerWidth <= 800) {
 		wrapper.addClass('mobile');
 	}
 
